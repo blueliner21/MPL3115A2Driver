04acaedfb34272255862223e95e28ee6b21b2bb2
Mike Morgan <blueliner21@gmail.com>
2024-06-03 12:06:00 -0600

    More Pre Release Fixes

--- MPL3115A2Driver/MPL3115A2Driver.py
+++ MPL3115A2Driver/MPL3115A2Driver.py
@@ -30,45 +30,26 @@ class MPL3115A2:
         self.i2c = i2c
         self.ADDR = ADDR
         self.ConnectAndVerify()
-    
-    def ConnectAndVerify(self): #ToDo: Connect and verify should be in a try loop
-        if  (self.i2c.readfrom_mem(self.ADDR,_WHO_AM_I,1))[0] == _device_id:
-            print("Comms verified. Chip model number confirmed")
-        else:
-            print("No Comms or Bad Chip") #ToDo: This should raise an exception 
-    
-    def TestDeviceAltimeter(self):
-        self.i2c.writeto(self.ADDR,bytearray([_PT_DATA_CFG,0x07])) ##Enable Data Flags
-        self.i2c.writeto(self.ADDR,bytearray([_CTRL_REG1,0xB9])) ##Altimeter Mode, 128 OSR, Active with aquisition every 1 second         
-        STA = self.i2c.readfrom_mem(self.ADDR,0x00,1) 
-        while(not((STA[0]) & (0x08))): ##Check to see if measurement is completed
-            #time.sleep_ms(500)
-            STA = self.i2c.readfrom_mem(self.ADDR,0x00,1)
-        self.i2c.writeto(self.ADDR,bytes([0x04]),False)
-        data = self.i2c.readfrom(0x60,2)
-        MSB = data[0]
-        LSB = data[1]
-        temp = (MSB<<8) | (LSB)
-        temp = temp>>4
-        temp = temp/16.0
-        print(temp)
-        self.i2c.writeto(self.ADDR,bytearray([_CTRL_REG1,0x38])) ##Altimeter Mode, 128 OSR, On Standby
 
+    def ConnectAndVerify(self): 
+        assert (self.i2c.readfrom_mem(self.ADDR,_WHO_AM_I,1))[0] == _device_id, "Chip ID Not Found. Check Comms"
+    
+    
     def GetStationPressure(self):
         """Obtain Station Pressure from MPL3115A2. Return a dict of station pressure in millibars, inHG, and PSI
         Return Dict Keys are: 'station_pressure_mBar', 'station_pressure_inHg', 'station_pressure_PSI'
         """
-        self.i2c.writeto(self.ADDR,bytearray([_PT_DATA_CFG,0x07])) ##Enable Data Flags
-        self.i2c.writeto(self.ADDR,bytearray([_CTRL_REG1,0x39])) ##Barometer Mode, 128 OSR, Active with aquisition every 1 second         
+        self.i2c.writeto(self.ADDR,bytearray([_PT_DATA_CFG,0x07])) 
+        self.i2c.writeto(self.ADDR,bytearray([_CTRL_REG1,0x39]))          
         STA = self.i2c.readfrom_mem(self.ADDR,0x00,1) 
-        while(not((STA[0]) & (0x08))): ##Check to see if measurement is completed
-            #time.sleep_ms(10)
+        while(not((STA[0]) & (0x08))): 
             STA = self.i2c.readfrom_mem(self.ADDR,0x00,1)
         
-        data = self.i2c.readfrom_mem(0x60,_OUT_P_MSB,3) #Get three bytes starting at _OUT_P_MSB register
+        data = self.i2c.readfrom_mem(0x60,_OUT_P_MSB,3) 
         MSB = data[0]
         CSB = data[1]
         LSB = data[2]
+        print(f'MSB = {MSB} CSB = {CSB} LSB = {LSB}')
         station_pressure_Pa = ((MSB<<24) | (CSB<<16) | (LSB<<8)) / 16384.0
         station_pressure_mBar = station_pressure_Pa/100
         station_pressure_inHg = station_pressure_Pa/3386
@@ -80,28 +61,31 @@ class MPL3115A2:
     def GetSLP(self):
         """
         Calculate Sea Level Pressure from MPL3115A2 station pressure. Return a dict of SLP in millibars, inHG, and PSI.
-        Station pressure is read from sensor along with temperature. SLP is approximated by:
+        Station pressure, station temp, and station altitude are read from the sensor. SLP is then approximated by:
         SLP = Station_Pressure * exp((_g*_STA_ALTITUDE)/(_Rd*tempK))
         Return Dict Keys: 'SLP_mBar', 'SLP_inHg', SLP_PSI'
         """
-        self.i2c.writeto(self.ADDR,bytearray([_PT_DATA_CFG,0x07])) ##Enable Data Flags
-        self.i2c.writeto(self.ADDR,bytearray([_CTRL_REG1,0x39])) ##Barometer Mode, 128 OSR, Active with aquisition every 1 second         
+        self.i2c.writeto(self.ADDR,bytearray([_PT_DATA_CFG,0x07])) 
+        self.i2c.writeto(self.ADDR,bytearray([_CTRL_REG1,0x39]))         
         STA = self.i2c.readfrom_mem(self.ADDR,0x00,1) 
-        while(not((STA[0]) & (0x08))): ##Check to see if measurement is completed
-            #time.sleep_ms(500)
+        while(not((STA[0]) & (0x08))): 
             STA = self.i2c.readfrom_mem(self.ADDR,0x00,1)
         
-        data = self.i2c.readfrom_mem(0x60,_OUT_P_MSB,5) #Get three bytes starting at _OUT_P_MSB register
+        data = self.i2c.readfrom_mem(0x60,_OUT_P_MSB,5) 
         Press_MSB = data[0]
         Press_CSB = data[1]
         Press_LSB = data[2]
         Temp_MSB = data[3]
         Temp_LSB = data[4]
+        
         station_pressure_Pa = ((Press_MSB<<24) | (Press_CSB<<16) | (Press_LSB<<8)) / 16384.0
         station_pressure_mBar = station_pressure_Pa/100
         temp_c = ((Temp_MSB << 8) | Temp_LSB) / 256.0
         temp_k = temp_c + 273.15
-        exp = ((_g*_STATION_ALTITUDE)/(_Rd*temp_k))
+
+        station_altitude = self.GetAltitude()
+
+        exp = ((_g*station_altitude["alt_meters"])/(_Rd*temp_k))
         SLP_mBar = station_pressure_mBar * math.exp(exp)
         SLP_inHg = SLP_mBar/33.864
         SLP_PSI = SLP_mBar/68.948
@@ -112,15 +96,14 @@ class MPL3115A2:
         Obtain SLP adjusted altitude. Return a dict of altitudes in meters, and feet
         Return Dict Keys: 'alt_meters', 'alt_feet'
         """
-        self.i2c.writeto(self.ADDR,bytearray([_PT_DATA_CFG,0x07])) ##Enable Data Flags
-        self.i2c.writeto(self.ADDR,bytearray([_CTRL_REG1,0xB9])) ##Altimeter Mode, 128 OSR, Active with aquisition every 1 second         
+        self.i2c.writeto(self.ADDR,bytearray([_PT_DATA_CFG,0x07])) 
+        self.i2c.writeto(self.ADDR,bytearray([_CTRL_REG1,0xB9]))         
         STA = self.i2c.readfrom_mem(self.ADDR,0x00,1) 
-        while(not((STA[0]) & (0x08))): ##Check to see if measurement is completed
-            #time.sleep_ms(500)
+        while(not((STA[0]) & (0x08))):
             STA = self.i2c.readfrom_mem(self.ADDR,0x00,1)
 
         
-        data = self.i2c.readfrom_mem(0x60,_OUT_P_MSB,3) #Get three bytes starting at _OUT_P_MSB register
+        data = self.i2c.readfrom_mem(0x60,_OUT_P_MSB,3) 
         ALT_MSB = data[0]
         ALT_CSB = data[1]
         ALT_LSB = data[2]
@@ -135,11 +118,10 @@ class MPL3115A2:
         Obtain temperature data. Return a dict of temperatures in degrees_c, degrees_f, and degrees_k
         Return Dict Keys: 'temp_c', 'temp_f', 'temp_k'
         """
-        self.i2c.writeto(self.ADDR,bytearray([_PT_DATA_CFG,0x07])) ##Enable Data Flags
-        self.i2c.writeto(self.ADDR,bytearray([_CTRL_REG1,0x39])) ##Barometer Mode, 128 OSR, Active with aquisition every 1 second         
+        self.i2c.writeto(self.ADDR,bytearray([_PT_DATA_CFG,0x07])) 
+        self.i2c.writeto(self.ADDR,bytearray([_CTRL_REG1,0x39]))       
         STA = self.i2c.readfrom_mem(self.ADDR,0x00,1) 
-        while(not((STA[0]) & (0x08))): ##Check to see if measurement is completed
-            #time.sleep_ms(500)
+        while(not((STA[0]) & (0x08))): 
             STA = self.i2c.readfrom_mem(self.ADDR,0x00,1)
         
         data = self.i2c.readfrom_mem(0x60,_OUT_T_MSB,2) 
@@ -163,11 +145,12 @@ if __name__ == "__main__":
     i2c = I2C(ID, scl = Pin(SCL), sda = Pin(SDA), freq=100000)
     altimeter = MPL3115A2(i2c, ADDR)
     for i in range(10):
-        pressure = altimeter.GetSLP()
+        #pressure = altimeter.GetSLP()
+        pressure = altimeter.GetStationPressure()
         altitude = altimeter.GetAltitude()
         temp = altimeter.GetTemp()
-        print(f'Pressure: {pressure["SLP_inHg"]:.2f} inHg Altitude: {altitude["alt_meters"]:.2f} meters Temp: {temp["temp_f"]:.2f} degrees F')
-        
+        #print(f'Pressure: {pressure["SLP_mBar"]:.2f} inHg Altitude: {altitude["alt_meters"]:.2f} meters Temp: {temp["temp_f"]:.2f} degrees F')
+        print(f'Pressure: {pressure["station_pressure_mBar"]:.2f} inHg Altitude: {altitude["alt_meters"]:.2f} meters Temp: {temp["temp_f"]:.2f} degrees F')
     
     
 
